3a23bce442c5b6973950e87ec127335d891a1747
Jason Mize <jasonemize@gmail.com>
2016-10-07 09:28:28 -0400

    post works

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 'use strict';
 
-function MainController () {
-    const ctrl = this;
-    ctrl.recentlySaved = false;
+function MainController ($scope) {
+    // const ctrl = this;
+ 
+    $scope.savedTasks = [
+            {
+                info : "I'd like to make a new post.",
+            },    
+            {
+                info : "I'd like the post to appear below this one.",
+            },                
+        ];
 
 
+    // ctrl.showTaskSaved = false; // visibility for save verification message
 
 
+    // save user input for new todo task
+     $scope.addSavedTask = function() {
+        $scope.savedTasks.push($scope.newTask);
+        $scope.newTask = {};
+
+        // ctrl.showTaskSaved = true; // visibility for save verification message
+
+        // $timeout(function() {
+        //     ctrl.showTaskSaved = false; // visibility for save verification message
+        // }, 2000);
+    }
 
 
 
@@ -15,6 +35,6 @@ function MainController () {
 
 }
 
-angular.module('app' [])
+angular.module('app', [])
     .controller('MainCtrl', MainController);
 
