d1447ca72141f05da8b712c132861aed57fa7805
Jason Mize <jasonemize@gmail.com>
2016-10-11 14:41:25 -0400

    edit has a direct binding that works

--- app.js
+++ app.js
@@ -3,16 +3,17 @@
 function MainController (orderByFilter, $timeout) {
     const ctrl = this;
 
-    var savedTasks = [ ];
+    var savedTasks = [ ]; // dictionary object to hold task info
+    ctrl.savedTasks = savedTasks; 
  
-    ctrl.order = 'dateAdded';
-    ctrl.reverse = false;
-    ctrl.completeStyle = 'not-complete';
-    ctrl.savedTasks = savedTasks;
+    ctrl.order = 'dateAdded'; // what field to order by at init
+    ctrl.reverse = false;  // init with field order not reversed
+    ctrl.completeStyle = 'not-complete';  // init with task given 'not-complete' style
     ctrl.showTaskSaved = false; // visibility for save verification message
-    ctrl.rightNow = Date.now();
+    ctrl.rightNow = Date.now(); // grab the current date
 
 
+    // order savedTasks by selected key
     ctrl.sortBy = function(order) {
         ctrl.reverse = (ctrl.order === order) ? !ctrl.reverse : false;
         ctrl.order = order;
@@ -20,22 +21,31 @@ function MainController (orderByFilter, $timeout) {
     };
 
 
+    // mark a task complete/uncomplete
     ctrl.toggleCompleteTask = function (task) {
         task.complete = !task.complete;
     };
 
 
+    // delete a task
     ctrl.deleteTask = function (index) {
         ctrl.savedTasks.splice(index, 1);
     };
 
 
-    // ctrl.editTask = function (index, key) {
-    //     ctrl.savedTasks[index] = "";
-    // };
+    // edit existing task
+    ctrl.editTask = function () {
+        // console.log (task);
+        // console.log (task.info)
+        // console.log(ctrl.savedTasks[index].info)
+        // console.log(ctrl.update.info)
+        // ctrl.savedTasks[index].info = ctrl.task.info;
+    };
 
 
-     ctrl.addSavedTask = function() {
+    // save new tasks to savedTasks
+    ctrl.addSavedTask = function() {
+        
         // set new tasks to uncomplete
         ctrl.newTask.complete = false;
 
@@ -46,6 +56,7 @@ function MainController (orderByFilter, $timeout) {
         if (!ctrl.newTask.dateDue) {
             ctrl.newTask.dateDue = ctrl.rightNow;
         }
+
         // save the user input
         ctrl.savedTasks.push(ctrl.newTask);
         ctrl.newTask = {};
@@ -59,6 +70,7 @@ function MainController (orderByFilter, $timeout) {
 
 
 }
+
 
 angular.module('app', [])
     .controller('MainCtrl', MainController);
